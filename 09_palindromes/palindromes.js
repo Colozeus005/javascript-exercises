ca88367a292a12d6446fc26e08b839a7d8015dc3
Coluzeus09 <146698023+Colozeus005@users.noreply.github.com>
2023-10-27 18:09:27 +0800

    finished calculator & palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const processedString1 = string.toLowerCase().replace(/[a-z0-9]/g, "")
+    return processedString1 ===  processedString1.split("").reverse().join("")
 };
+//string , " " > lower
 
 // Do not edit below this line
 module.exports = palindromes;
