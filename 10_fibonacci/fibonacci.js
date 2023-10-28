ad281f0145d64516e42211e51bda94cb97194099
Coluzeus09 <146698023+Colozeus005@users.noreply.github.com>
2023-10-28 21:16:59 +0800

    finish fibonacci & getTitles

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,20 +1,24 @@
-const fibonacci = function() {
-    if (count < 0) return "OOPS";
-    if (count === 0) return 0;
+const fibonacci = function(count) {
+    if (count < 0 ) return "Error"
+    if (count === 0) return 0
 
-    let firstPrev = 1;
-    let secondPrev = 0;
-    
-    for (let i = 2; i <= count; i++) {
-        let current = firstPrev + secondPrev;
-        secondPrev = firstPrev;
-        firstPrev = current;
+
+    let firstPrev= 1
+    let secondPrev = 0
+
+    for (let i = 2; i <= count; i++){
+        let current = firstPrev + secondPrev
+        secondPrev = firstPrev
+        firstPrev = current
     }
+    return firstPrev
 
-    return firstPrev;
 };
 
 ;
-
+// input 1-10 , not negative signs
+//output 1 , 1 , 2, 3 , 5, 8, 13,
+// 1 , 1, 2, 3, 5, 
+// 
 // Do not edit below this line
 module.exports = fibonacci;
